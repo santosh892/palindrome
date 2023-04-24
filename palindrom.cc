98de61ad78b8afd5c9d152216a95c40e9cace665
santosh chhetri <santoshchhetri234@gmail.com>
2023-04-24 19:51:55 +0545

    add java and c++

---
+++ palindrom.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+      printf("print(\"Hello world\")
+print('To check palindrome or not 👇👇')
+
+n=int(input(\"Enter number:\"))
+temp=n
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+if(temp==rev):
+    print(\"The number is a palindrome!\")
+else:
+    print(\"The number isn't a palindrome!\")
+
+    
+    print('created by santosh chhetri')\n");
+      return 0;
+}
